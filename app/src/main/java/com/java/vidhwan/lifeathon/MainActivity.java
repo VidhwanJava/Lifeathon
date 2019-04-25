898c58c9096f8c76db8f24a173c9586545bf9dff
Vidhwan Java <vidhwanjav@gmail.com>
2019-04-25 23:07:53 +0530

    Initial commit

---
+++ app/src/main/java/com/java/vidhwan/lifeathon/MainActivity.java
@@ -0,0 +1,210 @@
+package com.java.vidhwan.lifeathon;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.ScrollView;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private Animator mCurrentAnimatorEffect;
+    private int mShortAnimationDurationEffect;
+    Button bt1,bt2,bt3,bt4;
+    ScrollView p1,p2,p3,p4;
+    View current;
+    VideoView vview;
+    MediaController mediac;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS,WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        setContentView(R.layout.activity_main);
+        bt1=findViewById(R.id.bt1);
+        bt2=findViewById(R.id.bt2);
+        bt3=findViewById(R.id.bt3);
+        bt4=findViewById(R.id.bt4);
+        p1=findViewById(R.id.p1);
+        p2=findViewById(R.id.p2);
+        p3=findViewById(R.id.p3);
+        p4=findViewById(R.id.p4);
+        current=p1;
+        vview = (VideoView)findViewById(R.id.testved);
+        mediac= new MediaController(this);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.test;
+        vview.setVideoURI(Uri.parse(path));
+        vview.setMediaController(mediac);
+        mediac.setAnchorView(vview);
+//        vview.start();
+
+
+        final View thumbImageView = findViewById(R.id.imageView5);
+        thumbImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zoomImageFromThumb(thumbImageView, R.drawable.robo2);
+            }
+        });
+
+        mShortAnimationDurationEffect = getResources().getInteger(
+                android.R.integer.config_shortAnimTime);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void zoomImageFromThumb(final View thumbView, int imageResId) {
+        if (mCurrentAnimatorEffect != null) {
+            mCurrentAnimatorEffect.cancel();
+        }
+
+        final ImageView expandedImageView = (ImageView) findViewById(
+                R.id.expv);
+        expandedImageView.setImageResource(imageResId);
+
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.linearLayout)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
+                View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDurationEffect);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimatorEffect = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimatorEffect = null;
+            }
+        });
+        set.start();
+        mCurrentAnimatorEffect = set;
+
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimatorEffect != null) {
+                    mCurrentAnimatorEffect.cancel();
+                }
+
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y,startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDurationEffect);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimatorEffect = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimatorEffect = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimatorEffect = set;
+            }
+        });
+    }
+
+
+
+    public void bt1o(View view){
+        current.setVisibility(View.GONE);
+        p1.setVisibility(View.VISIBLE);
+        current=p1;
+    }
+
+    public void bt2o(View view){
+        current.setVisibility(View.GONE);
+        p2.setVisibility(View.VISIBLE);
+        current=p2;
+        vview.pause();
+    }
+
+    public void bt3o(View view){
+        current.setVisibility(View.GONE);
+        p3.setVisibility(View.VISIBLE);
+        current=p3;
+    }
+
+    public void bt4o(View view){
+        current.setVisibility(View.GONE);
+        p4.setVisibility(View.VISIBLE);
+        current=p4;
+    }
+}
